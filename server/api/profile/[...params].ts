0522620d28aab631a478171ae1d9870203abaf8c
Hayden Bradfield <hbradfield20@gmail.com>
2023-08-23 16:50:04 -0400

    wip

--- server/api/profile/[...params].ts
+++ server/api/profile/[...params].ts
@@ -1,9 +1,10 @@
-import { createRouter, useBase, defineEventHandler, getQuery } from 'h3';
+import { createRouter, useBase, defineEventHandler, getQuery, parseCookies } from 'h3';
 
 const router = createRouter();
 const runtimeConfig = useRuntimeConfig();
 const accountAPIUri = runtimeConfig.account_api_uri;
 const accountAPIKey = runtimeConfig.account_api_key;
+const jwtSecret = runtimeConfig.jwt_secret;
 
 router.get('/profile-exists', defineEventHandler(async (event) => {
 
@@ -41,10 +42,35 @@ router.get('/public', defineEventHandler(async (event) => {
             }
         });
 
-        console.log(data);
-
         return data;
 
+
+    }catch(e){
+        console.error(e);
+        return ResponseError.respondWithError(e);
+    }
+
+
+}));
+
+router.get('/auth-user', defineEventHandler(async (event) => {
+
+    try{
+
+        const query = getQuery(event);
+        const profileUserAccountName = query['profile_user_account_name'];
+        const cookies = parseCookies(event);
+        const authData = evalToken(cookies['Authorization'], jwtSecret);
+
+        const response: any = $fetch(`${accountAPIUri}/social/profile/auth-user?auth_user_account_name=${authData.account_name}&profile_account_name=${profileUserAccountName}`, {
+            headers: {
+                'Content-Type': 'application/json', 
+                'Accept': 'application/json', 
+                'x-api-key': accountAPIKey
+            }
+        })
+
+        return response;
 
     }catch(e){
         console.error(e);
